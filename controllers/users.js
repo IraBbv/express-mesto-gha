40c4fa911ffeff40980e667d8f871bdf5b9cb01d
Irina Bibaeva <irabbv@yandex.ru>
2023-11-22 05:29:48 +0300

    createUser + login controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports.getAllUsers = (req, res) => {
@@ -25,10 +28,25 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    email, password, name, about, avatar,
+  } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+      about,
+      avatar,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `Переданы некорректные данные: ${err.message}` });
@@ -73,5 +91,22 @@ module.exports.updateUserAvatar = (req, res) => {
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'Some-secret-key',
+        { expiresIn: '7d' },
+      );
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
     });
 };
--- models/user.js
+++ models/user.js
@@ -1,17 +1,32 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      required: [true, 'Обязательное поле'],
+    email: {
       type: String,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Введите почту',
+      },
+      required: [true, 'Обязательное поле'],
+      unique: [true, 'Пользователь с такой почтой уже существует'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Обязательное поле'],
+      minlength: [8, 'Минимальная длина пароля - 8 знака'],
+    },
+    name: {
+      type: String,
+      defaul: 'Жак-Ив Кусто',
       minlength: [2, 'Минимальная длина поля - 2 знака'],
       maxlength: [30, 'Максимальная длина поля - 30 знаков'],
     },
     about: {
-      required: [true, 'Обязательное поле'],
       type: String,
+      defaul: 'Исследователь',
       minlength: [2, 'Минимальная длина поля - 2 знака'],
       maxlength: [30, 'Максимальная длина поля - 30 знаков'],
     },
@@ -21,10 +36,26 @@ const userSchema = new mongoose.Schema(
         validator: (v) => validator.isURL(v),
         message: 'Некорректный URL',
       },
-      required: [true, 'Обязательное поле'],
+      defaul: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
   },
   { versionKey: false },
 );
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неверные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неверные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
