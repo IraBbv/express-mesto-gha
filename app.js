9218ac8a3c7e0fe74c99de1abe0bc0ad401618d6
Irina Bibaeva <irabbv@yandex.ru>
2023-12-03 00:24:33 +0300

    review fixes v4.6

--- app.js
+++ app.js
@@ -52,8 +52,9 @@ app.post('/signin', celebrate({
   }).unknown(true),
 }), login);
 
-app.use('/users', auth, require('./routes/users'));
-app.use('/cards', auth, require('./routes/cards'));
+app.use(auth);
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use('*', (req, res) => {
   res.send(new NotFoundError('Страница не найдена'));
